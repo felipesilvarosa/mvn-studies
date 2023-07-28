0828f3a9db2f756d0636fae29dc32c56fc3202bf
Felipe Rosa <felipe.rosaa@mirante.net.br>
2023-07-28 14:58:52 -0300

    chore:  Add StringUtils Dep to Project

--- hello-world/src/main/java/rosa/felipe/Main.java
+++ hello-world/src/main/java/rosa/felipe/Main.java
@@ -1,7 +1,9 @@
 package rosa.felipe;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
