af0221ffd093320046057e15da04ff9f9987ddbb
Felipe Rosa <felipesrosa@gmail.com>
2023-08-01 15:45:07 -0300

    chore: mvn spring boot multi module

--- mvn-spring/web-app/src/main/java/rosa/felipe/controller/ExampleController.java
+++ mvn-spring/web-app/src/main/java/rosa/felipe/controller/ExampleController.java
@@ -1,4 +1,4 @@
-package rosa.felipe.mvnspring.controller;
+package rosa.felipe.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
