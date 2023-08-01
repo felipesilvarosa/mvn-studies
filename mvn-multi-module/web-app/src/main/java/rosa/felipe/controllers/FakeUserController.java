980ebc6fcaed342930535be552097640252b4b61
Felipe Rosa <felipesrosa@gmail.com>
2023-08-01 11:09:07 -0300

    chore: create module web app

---
+++ mvn-multi-module/web-app/src/main/java/rosa/felipe/controllers/FakeUserController.java
@@ -0,0 +1,11 @@
+package rosa.felipe.controllers;
+
+import rosa.felipe.entities.User;
+
+public class FakeUserController {
+
+    //fake method to multi module sample
+    User saveUser(User user) {
+        return user;
+    }
+}
